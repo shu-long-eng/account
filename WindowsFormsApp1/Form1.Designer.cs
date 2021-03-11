20b3d2a002d4b8a5dd33e237d73bc0d6297d7a38
Hsu <qw9596128@gmail.com>
2021-03-11 14:36:46 +0800

    加入專案檔案。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lable = new System.Windows.Forms.Label();
+            this.Date = new System.Windows.Forms.Label();
+            this.cost = new System.Windows.Forms.Label();
+            this.Money = new System.Windows.Forms.Label();
+            this.Use = new System.Windows.Forms.Label();
+            this.Description = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lable
+            // 
+            this.lable.AutoSize = true;
+            this.lable.Font = new System.Drawing.Font("新細明體", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lable.Location = new System.Drawing.Point(324, 21);
+            this.lable.Name = "lable";
+            this.lable.Size = new System.Drawing.Size(185, 33);
+            this.lable.TabIndex = 0;
+            this.lable.Text = "現金日記帳";
+            // 
+            // Date
+            // 
+            this.Date.AutoSize = true;
+            this.Date.Font = new System.Drawing.Font("新細明體", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Date.Location = new System.Drawing.Point(111, 91);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(62, 25);
+            this.Date.TabIndex = 1;
+            this.Date.Text = "日期";
+            // 
+            // cost
+            // 
+            this.cost.AutoSize = true;
+            this.cost.Font = new System.Drawing.Font("新細明體", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cost.Location = new System.Drawing.Point(111, 165);
+            this.cost.Name = "cost";
+            this.cost.Size = new System.Drawing.Size(119, 25);
+            this.cost.TabIndex = 2;
+            this.cost.Text = "支出/收入";
+            // 
+            // Money
+            // 
+            this.Money.AutoSize = true;
+            this.Money.Font = new System.Drawing.Font("新細明體", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Money.Location = new System.Drawing.Point(111, 226);
+            this.Money.Name = "Money";
+            this.Money.Size = new System.Drawing.Size(62, 25);
+            this.Money.TabIndex = 3;
+            this.Money.Text = "金額";
+            // 
+            // Use
+            // 
+            this.Use.AutoSize = true;
+            this.Use.Font = new System.Drawing.Font("新細明體", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Use.Location = new System.Drawing.Point(111, 300);
+            this.Use.Name = "Use";
+            this.Use.Size = new System.Drawing.Size(62, 25);
+            this.Use.TabIndex = 4;
+            this.Use.Text = "科目";
+            // 
+            // Description
+            // 
+            this.Description.AutoSize = true;
+            this.Description.Font = new System.Drawing.Font("新細明體", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Description.Location = new System.Drawing.Point(111, 363);
+            this.Description.Name = "Description";
+            this.Description.Size = new System.Drawing.Size(62, 25);
+            this.Description.TabIndex = 5;
+            this.Description.Text = "摘要";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(293, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(272, 25);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(293, 226);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(402, 25);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(293, 299);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(402, 25);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(293, 362);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(402, 119);
+            this.textBox5.TabIndex = 10;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(293, 165);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(402, 23);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(293, 91);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 12;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(620, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "選擇";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(672, 507);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 14;
+            this.Save.Text = "儲存";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(582, 507);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 15;
+            this.Exit.Text = "離開";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 551);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Description);
+            this.Controls.Add(this.Use);
+            this.Controls.Add(this.Money);
+            this.Controls.Add(this.cost);
+            this.Controls.Add(this.Date);
+            this.Controls.Add(this.lable);
+            this.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lable;
+        private System.Windows.Forms.Label Date;
+        private System.Windows.Forms.Label cost;
+        private System.Windows.Forms.Label Money;
+        private System.Windows.Forms.Label Use;
+        private System.Windows.Forms.Label Description;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("支出");
+            comboBox1.Items.Add("收入");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            monthCalendar1.Hide(); //開啟程式馬上隱藏日曆
+
+            textBox1.ReadOnly = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            monthCalendar1.Show(); //按下選擇鍵顯示日曆
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            textBox1.Text = monthCalendar1.SelectionStart.ToShortDateString(); //選擇日期
+            monthCalendar1.Hide(); //選擇日期後隱藏日曆
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0); //離開程式
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            string con, sql;
+            con = "Data Source=.\\SQLExpress;Initial Catalog=財經系統;Integrated Security=True"; //連接資料庫
+
+            sql = "insert into moneydata   values ('" + textBox1.Text+"\',"+
+                                                    "\'"+comboBox1.Text+"\',"+
+                                                          textBox3.Text+","+
+                                                      "\'"+textBox4.Text+"\',"+
+                                                         "\'"+textBox5.Text+
+                                                                                 " ')"; //SQL 語法
+            
+            SqlConnection mycon = new SqlConnection(con);
+
+            mycon.Open();
+
+            SqlDataAdapter myda = new SqlDataAdapter(sql, con);
+            
+            DataSet myds = new DataSet();
+
+            if (textBox1.Text == "") { MessageBox.Show("日期不能為空"); }
+            else if (comboBox1.Text == "") { MessageBox.Show("請選擇收入/支出"); }
+            else if (textBox3.Text == "") { MessageBox.Show("金額不能為空"); }
+            else if (textBox4.Text == "") { MessageBox.Show("用途不能為空"); }
+            //else if (textBox5.Text == "") { MessageBox.Show("用途不能為空"); }
+            else
+            {
+                myda.Fill(myds, "財經系統");
+                
+                mycon.Close();
+                MessageBox.Show("儲存完畢");
+                System.Environment.Exit(0); //離開程式
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+       private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((int)e.KeyChar < 48 | (int)e.KeyChar > 57) & (int)e.KeyChar != 8) //KeyChar字碼內48-57為數字 8為倒退鍵
+            {
+                e.Handled = true; //跳過處理
+            }
+        }
+    }
+}
